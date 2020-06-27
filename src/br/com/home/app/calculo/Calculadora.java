b1d671be9cd6c504e408d8f473c4e5e2e8a719fd
Douglas Vitor França da Silva <douglas.vitor@totvs.com.br>
2020-06-27 19:00:52 -0300

    357. Requires Transitive

--- src/br/com/home/app/calculo/Calculadora.java
+++ src/br/com/home/app/calculo/Calculadora.java
@@ -1,13 +1,20 @@
 package br.com.home.app.calculo;
 
+import static br.com.home.app.logging.AppLogger.info;
 import br.com.home.app.calculo.interno.OperacoesAritmeticas;
+import br.com.home.app.logging.AppLogger;
 
 public class Calculadora {
 	
 	private OperacoesAritmeticas opAritmeticas = new OperacoesAritmeticas();
 
 	public double soma(double... nums) {
+		info("Somando... ");
 		return opAritmeticas.soma(nums);
+	}
+	
+	public Class<AppLogger> getLoggerClass(){
+		return AppLogger.class;
 	}
 	
 }
--- src/module-info.java
+++ src/module-info.java
@@ -1,5 +1,6 @@
 module app.calculo {
 	
+	requires transitive app.logging;
 	exports br.com.home.app.calculo;
 	
 }
